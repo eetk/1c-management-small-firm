28baaed97308299a90c4ce707371a6f1a92915e6
Alexander Plesovskikh <me@hgen.ru>
2013-12-09 07:36:35 +0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'introduction'
 
 # General information about the project.
 project = u'1C: Management a small firm'
