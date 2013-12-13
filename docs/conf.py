eb00a310d4e171ad5612a6409c160457bbe17078
Alexandr Plesovskih <aplesovskih@naumen.ru>
2013-12-13 20:31:53 +0600

    asd

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'introduction'
+master_doc = 'index'
 
 # General information about the project.
 project = u'1C: Management a small firm'
